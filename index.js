95f7977e4ff1aac45d6290c467e7341077c7bff3
kharoteswati <kharoteswati31@gmail.com>
2023-06-12 17:07:20 +0530

    fghg

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const { userRouter } = require("./routes/userRoutes");
 const { connection } = require("./db")
 const { postRoutes } = require("./routes/postRoutes");
 require("dotenv").config();
+const cors = require("cors")
 
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 app.use("/users", userRouter)
 app.use("/posts", postRoutes)
 
